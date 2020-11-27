012bf5d1aed3fd166aa0640ad1db5397e3b37370
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-11-27 12:11:39 -0700

    HTML and API routes started

--- js/server.js
+++ js/server.js
@@ -6,3 +6,17 @@ var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//HTML Routes
+app.get("/", function(req, res) {
+res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+//API Route
+app.get("/api/notes", function(req, res) {
+return res.json(____);
+});
