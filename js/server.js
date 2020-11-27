467ebb8664d58349255c11c566662266de3a8fb6
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2020-11-27 12:18:51 -0700

    Get, post, and delete route skeletons added for /api/notes and listen added to start server

--- js/server.js
+++ js/server.js
@@ -8,15 +8,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //HTML Routes
-app.get("/", function(req, res) {
-res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/notes", function(req, res) {
-res.sendFile(path.join(__dirname, "notes.html"));
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
 });
 
 //API Route
-app.get("/api/notes", function(req, res) {
-return res.json(____);
+app.get("/api/notes", function (req, res) {
+    //Reads JSON file
+    //Returns all saved notes as JSON
+    //return res.json(____);
+});
+
+app.post("/api/notes", function (req, res) {
+    //Receives new note to save on body
+    //Adds to db.json file
+    //Returns new note to client
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    //Receive query parameter containing ID of note to delete
+    //Each note needs unique ID
+    //Need to read all notes from db.json (for loop), and remove note with given ID
+    //Rewrite the notes to db.json file
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
