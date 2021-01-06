66d0c3ecbe3a6e7b976284c23f9fc751bbcadbcd
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-05 21:02:08 -0700

    Installed FS NPM, created path to db.json file, readFileSync method added to read file and return notes, writeFile methods started for POST and DELETE methods

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "db");
+const outputPath = path.join(OUTPUT_DIR, "db.json");
 
 var app = express();
 var PORT = 3000;
@@ -19,6 +23,11 @@ app.get("/notes", function (req, res) {
 //API Route
 app.get("/api/notes", function (req, res) {
     //Reads JSON file
+    var notes = fs.readFileSync(outputPath, "utf8", (err, data) => {
+        if (err) throw (err);
+        console.log(data);
+    });
+    console.log(notes);
     //Returns all saved notes as JSON
     return res.json(notes);
 });
@@ -26,7 +35,9 @@ app.get("/api/notes", function (req, res) {
 app.post("/api/notes", function (req, res) {
     //Receives new note to save on body
     //Adds to db.json file
+    fs.writeFile(path.join(outputPath, "db.json"));
     //Returns new note to client
+    return res.json(note);
 });
 
 app.delete("/api/notes/:id", function (req, res) {
@@ -34,6 +45,7 @@ app.delete("/api/notes/:id", function (req, res) {
     //Each note needs unique ID
     //Need to read all notes from db.json (for loop), and remove note with given ID
     //Rewrite the notes to db.json file
+    fs.writeFile(path.join(outputPath, "db.json"));
 });
 
 app.listen(PORT, function () {
