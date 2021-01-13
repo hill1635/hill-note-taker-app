2c07e9418435f21734b482cfc580fe969bc73e45
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-01-12 23:02:20 -0700

    Cleaned up comments, console.logs, and unnecessary code.

--- server.js
+++ server.js
@@ -11,24 +11,19 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//HTML Routes
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
+    res.sendFile(path.join(__dirname, "notes.html"));
 });
 
-//API Route
 app.get("/api/notes", function (req, res) {
-    //Reads JSON file
     var notes = fs.readFileSync(outputPath, "utf8", (err, data) => {
         if (err) throw (err);
-        console.log(data);
     });
     notes = JSON.parse(notes);
-    //Returns all saved notes as JSON
     return res.json(notes);
 });
 
@@ -37,18 +32,14 @@ app.post("/api/notes", function (req, res) {
 
     var notes = fs.readFileSync(outputPath, "utf8", (err, data) => {
         if (err) throw (err);
-        console.log(data);
     });
 
     notes = JSON.parse(notes);
     notes.push(newNote);
-    //Assign ID numbers
     for (i = 0; i < notes.length; i++) {
         var note = notes[i];
         note.id = i;
-        console.log(note);
     }
-    console.log("notes: ", notes);
 
     const writeFile = () => {
         notes = JSON.stringify(notes);
@@ -57,22 +48,15 @@ app.post("/api/notes", function (req, res) {
         });
     }
     writeFile();
-    //Returns new note to client
     return res.json(newNote);
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-    //Receive query parameter containing ID of note to delete
     var chosenID = req.params.id;
-    console.log(chosenID);
-    //Each note needs unique ID
-    //Need to read all notes from db.json (for loop), and remove note with given ID
     var notes = fs.readFileSync(outputPath, "utf8", (err, data) => {
         if (err) throw (err);
-        console.log(data);
     });
     notes = JSON.parse(notes);
-    console.log(notes);
 
     for (i = 0; i < notes.length; i++) {
         var note = notes[i];
@@ -80,16 +64,12 @@ app.delete("/api/notes/:id", function (req, res) {
             notes.splice(note.id, 1);
         }
     }
-    console.log("notes after removal: ", notes);
     notes = JSON.stringify(notes);
-    //Rewrite the notes to db.json file
-    // const writeFile = () => {
-    //     notes = JSON.stringify(notes);
+ 
     fs.writeFileSync(outputPath, notes, (err) => {
         if (err) throw (err);
     });
-    // }
-    // writeFile();
+
     return res.json(notes);
 });
 
