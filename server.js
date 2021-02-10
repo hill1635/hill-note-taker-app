902a23708022f9e603e158fe9321e1d2d1691b9f
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2021-02-10 15:29:47 -0700

    Added express static middleware to get html files in public folder.  Added html files to public folder and created assets folder with css and js subfolders.

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
